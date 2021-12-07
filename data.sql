de5150f4a5b68c35081cce3d05f73664cde8d099
yahya <crazy20046@gmail.com>
2021-12-07 03:36:42 +0200

    Create animals table

--- data.sql
+++ data.sql
@@ -1,5 +1,8 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES 
+	(1, 'hello', '2018-02-03', 0, TRUE, 4.3),
+  (2, 'vicky', '2021-02-03', 0, FALSE, 10.23),
+  (3, 'hero', '2019-02-12', 0, TRUE, 11.23),
+  (4, 'raad', '2020-02-02', 0, FALSE, 9.4);
