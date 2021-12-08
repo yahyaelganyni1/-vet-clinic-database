c42e8e522d03872330cf7e53fef2d292c3f4fd7b
yahya <crazy20046@gmail.com>
2021-12-08 08:47:24 +0200

    Update queries

--- queries.sql
+++ queries.sql
@@ -8,3 +8,43 @@ SELECT name, escape_attempts  FROM animals WHERE weight_kg > 10.5,
 SELECT name FROM animals WHERE neutered = TRUE,
 SELECT name FROM animals WHERE name = 'Gabumon',
 SELECT name FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+/* change species to unspecified and rollback */
+BEGIN;
+COMMIT;
+UPDATE animals SET species = 'unspecified';
+ROLLBACK;
+END;
+
+/* set animals with mon to digimon */
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+COMMIT;
+END;
+
+/* set pokemon for all null */
+BEGIN;
+UPDATE animals SET species = 'pokemon' WHERE species is NULL;
+COMMIT;
+END;
+
+
+/* delete all animals and rollback*/
+BEGIN;
+DELETE from animals *;
+SELECT * from animals;
+ROLLBACK;
+END;
+
+SELECT * from animals; /* making sure all data still exist */
+
+/* delete animals born after Jan 1st, 2022*/
+BEGIN;
+DELETE from animals where date_of_birth >= '2022-01-01';
+COMMIT;
+SAVEPOINT SP1;
+UPDATE animals SET weight_kg = weight_kg * - 1;
+ROLLBACK SP1;
+UPDATE animals SET weight_kg = weight_kg * - 1 where weight_kg < 0;
+COMMIT;
+END;
