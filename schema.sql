f2eaa524f0bc6c0efd10f398c73d34ee8dc211ed
isaka-lumato <75973193+isaka-lumato@users.noreply.github.com>
2021-12-15 11:31:45 +0300

    schema update

--- schema.sql
+++ schema.sql
@@ -1,5 +1,98 @@
+
 /* Database schema to keep the structure of entire database. */
+CREATE DATABASE vet_clinic;
 
 CREATE TABLE animals (
-    name varchar(100)
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100),
+    date_of_birth DATE,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg FLOAT,
+    PRIMARY KEY(id)
 );
+
+ALTER TABLE
+    animals
+ADD
+    species VARCHAR(100);
+
+/* Query multiple tables */
+CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR(100),
+    age INT,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100),
+    PRIMARY KEY(id)
+);
+
+ALTER TABLE
+    animals DROP COLUMN species;
+
+ALTER TABLE
+    animals
+ADD
+    species_id INT;
+
+ALTER TABLE
+    animals
+ADD
+    owner_id INT;
+
+ALTER TABLE
+    animals
+ADD
+    FOREIGN KEY (species_id) REFERENCES species(id);
+
+ALTER TABLE
+    animals
+ADD
+    FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+/* Add "join table" for visits */
+CREATE TABLE vets (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100),
+    age INT,
+    date_of_graduation DATE,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE specialization (
+    species_id INT,
+    vets_id INT,
+    FOREIGN KEY (species_id) REFERENCES species (id),
+    FOREIGN KEY (vets_id) REFERENCES vets (id),
+    PRIMARY KEY (species_id, vets_id)
+);
+
+CREATE TABLE visits (
+    animals_id INT,
+    vets_id INT,
+    date_of_visit DATE,
+    id INT GENERATED ALWAYS AS IDENTITY,
+    FOREIGN KEY (animals_id) REFERENCES animals (id),
+    FOREIGN KEY (vets_id) REFERENCES vets (id),
+    PRIMARY KEY (id)
+);
+
+/* Performace Audit */
+-- Add an email column to your owners table
+ALTER TABLE
+    owners
+ADD
+    COLUMN email VARCHAR(120);
+
+-- Optimize visits table by creating an Index using the animals_id column
+CREATE INDEX animals_id_asc ON visits (animals_id ASC);
+
+-- Optimize visits table by creating an Index using the vets_id column
+CREATE INDEX vets_id_asc ON visits (vets_id ASC);
+
+-- -- Optimize owners table by creating an Index using the email column
+CREATE INDEX owners_email_asc ON owners (email ASC);
