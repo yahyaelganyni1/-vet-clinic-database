4e4c25707e8b46e2bbc253e0242a9d0691911ccf
yahya <crazy20046@gmail.com>
2021-12-15 09:52:26 +0200

    update the data

--- data.sql
+++ data.sql
@@ -1,66 +1,150 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg, species)
-VALUES 
-	(1, 'Agumon', '2020-02-03', 0, TRUE, 10.23),
-  (2, 'Gabumon', '2018-11-15', 2, TRUE, 8),
-  (3, 'Pikachu', '2019-02-12', 1, FALSE, 15.04),
-  (4, 'Devimon', '2017-05-12', 5, TRUE, 11),
-  (5, 'Charmander', '2020-02-08', 0, FALSE, 11),
-	(6, 'Plantmon', '2022-11-15', 2, TRUE, 5.7),
-	(7, 'Squirtle', '1993-06-02', 3, FALSE, 12.13),
-	(8, 'Boarmon', '2005-06-17', 7, TRUE, 45),
-  (9, 'Blossom', '1998-10-13', 3, TRUE, 17);
-
-  -- Insert into owners table
-INSERT INTO owners (full_name, age) 
-  VALUES ('Sam Smith', 34),
-        ('Jennifer', 19),
-        ('Bob', 45),
-        ('Melody Pond', 77),
-        ('Dean Winchester', 14),
-        ('Jodie Whittaker', 38);
-
--- Insert into species table
-INSERT INTO species (name)
-  VALUES ('Pokemon'),
-        ('Digimon');
-
--- set species-id for name ends in "mon"
-BEGIN;
-UPDATE animals
-SET species_id = 1 WHERE name LIKE '%mon';
-COMMIT;
-
--- set poke
-BEGIN;
-UPDATE animals
-SET species_id = 2 WHERE species_id is NULL;
-COMMIT;
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (
+    'Agumon',
+    'Feb 3, 2020',
+    10.23,
+    true,
+    0
+);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (
+    'Gabumon',
+    'Nov 15, 2018',
+    8,
+    true,
+    2
+);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (
+    'Pikachu',
+    'Jan 7, 2021',
+    15.04,
+    false,
+    1
+);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (
+    'Devimon',
+    'May 12, 2017',
+    11,
+    true,
+    5
+);
 
 
--- Modify your inserted animals to include owner information (owner_id):
-BEGIN;
-UPDATE animals
-SET owners_id = (SELECT id FROM owners WHERE full_name = 'Sam Smith')  WHERE name='Agumon';
-COMMIT;
+/* Update and Query animals table */
 
-BEGIN;
-UPDATE animals
-SET owners_id = (SELECT id FROM owners WHERE full_name='Jennifer Orwell') WHERE name='Gabumon' OR name='Pikachu';
-COMMIT;
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (
+    'Charmander',
+    'Feb 8, 2020',
+    -11,
+    false,
+    0
+);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (
+    'Plantmon',
+    'Nov 15, 2022',
+    -5.7,
+    true,
+    2
+);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (
+    'Squirtle',
+    'Apr 2, 1993',
+    -12.13,
+    false,
+    3
+);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (
+    'Angemon',
+    'Jun 12, 2005',
+    -45,
+    true,
+    1
+);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (
+    'Boarmon',
+    'Jun 7, 2005',
+    20.4,
+    true,
+    7
+);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (
+    'Blossom',
+    'Oct 13, 1998',
+    17,
+    true,
+    3
+);
 
-BEGIN;
-UPDATE animals
-SET owners_id= (SELECT id FROM owners WHERE full_name='Bob') WHERE name='Devimon' OR name='Plantmon';
-COMMIT;
+/* Query multiple tables */
 
-BEGIN;
-UPDATE animals
-SET owners_id= (SELECT id FROM owners WHERE full_name='Melody Pond') WHERE name='Charmander' OR name='Squirtle' OR name='Blossom';
-COMMIT;
+BEGIN TRANSACTION;
+INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34);
+INSERT INTO owners (full_name, age) VALUES ('Jennifer Orwell', 19);
+INSERT INTO owners (full_name, age) VALUES ('Bob', 45);
+INSERT INTO owners (full_name, age) VALUES ('Melody Pond', 77);
+INSERT INTO owners (full_name, age) VALUES ('Dean Winchester', 14);
+INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', 38);
+COMMIT TRANSACTION;
 
-BEGIN;
-UPDATE animals
-SET owners_id= (SELECT id FROM owners WHERE full_name='Dean Winchester') WHERE name='Angemon' OR name='Boarmon';
-COMMIT;
+BEGIN TRANSACTION;
+INSERT INTO species (name) VALUES ('Pokemon');
+INSERT INTO species (name) VALUES ('Digimon');
+COMMIT TRANSACTION;
+
+BEGIN TRANSACTION;
+UPDATE animals SET species_id = 1 WHERE name LIKE '%mon';
+UPDATE animals SET species_id = 2 WHERE species_id IS NULL;
+COMMIT TRANSACTION;
+
+BEGIN TRANSACTION;
+UPDATE animals SET owner_id = 1 WHERE name LIKE 'Agumon';
+UPDATE animals SET owner_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
+UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
+COMMIT TRANSACTION;
+
+/* Add "join table" for visits */
+
+BEGIN TRANSACTION;
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('William Tatcher', 45, 'Apr 23, 2000');
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Maisy Smith', 26, 'Jan 17, 2019');
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Stephanie Mendez', 64, 'May 4, 1981');
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Jack Harkness', 38, 'Jun 8, 2008');
+COMMIT TRANSACTION;
+
+BEGIN TRANSACTION;
+INSERT INTO specialization (vets_id, species_id) VALUES (1, 1);
+INSERT INTO specialization (vets_id, species_id) VALUES (3, 2);
+INSERT INTO specialization (vets_id, species_id) VALUES (3, 1);
+INSERT INTO specialization (vets_id, species_id) VALUES (4, 2);
+COMMIT TRANSACTION;
+
+BEGIN TRANSACTION;
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (1, 1, 'May 24, 2020');
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (1, 3, 'Jul 22, 2020');
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (2, 4, 'Feb 2, 2021');
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (3, 2, 'Jan 5, 2020');
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (3, 2, 'Mar 8, 2020');
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (3, 2, 'May 14, 2020');
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (4, 3, 'May 4, 2021');
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (5, 4, 'Feb 24, 2021');
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (6, 2, 'Dec 21, 2019');
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (6, 1, 'Aug 10, 2020');
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (6, 2, 'Apr 7, 2021');
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (7, 3, 'Sep 29, 2019');
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (8, 4, 'Oct 3, 2020');
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (8, 4, 'Nov 4, 2020');
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (9, 2, 'Jan 24, 2019');
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (9, 2, 'May 15, 2019');
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (9, 2, 'Feb 27, 2020');
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (9, 2, 'Aug 3, 2020');
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (10, 3, 'May 24, 2020');
+INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (10, 1, 'Jan 11, 2021');
+COMMIT TRANSACTION;
+
+-- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
+INSERT INTO visits (animals_id, vets_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+-- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
+insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
--- queries.sql
+++ queries.sql
@@ -1,53 +1,144 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT name FROM animals  WHERE name LIKE '%mon',
-SELECT name, date_of_birth  FROM animals WHERE date_of_birth BETWEEN  '2016-01-01' AND '2019-01-01',
-SELECT name, neutered FROM animals WHERE neutered = TRUE AND escape_attempts < 3,
-SELECT name FROM animals WHERE name = 'Agumon' OR name = 'Pikachu',
-SELECT name, escape_attempts  FROM animals WHERE weight_kg > 10.5,
-SELECT name FROM animals WHERE neutered = TRUE,
-SELECT name FROM animals WHERE name = 'Gabumon',
-SELECT name FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+SELECT * from animals WHERE name LIKE '%mon';
 
-ALTER TABLE animals 
-ADD COLUMN species VARCHAR(50);
+SELECT name from animals WHERE EXTRACT(year FROM date_of_birth) BETWEEN 2016 and 2019;
 
-/* change species to unspecified and rollback */
-BEGIN;
-COMMIT;
+SELECT name from animals WHERE neutered IS TRUE AND escape_attempts < 3;
+
+SELECT date_of_birth from animals WHERE name IN ('Agumon', 'Pikachu');
+
+SELECT name, escape_attempts from animals WHERE weight_kg > 10.5;
+
+SELECT * from animals WHERE neutered IS TRUE;
+
+SELECT * from animals WHERE name NOT LIKE 'Gabumon';
+
+SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+/* Query and Update animal table */
+BEGIN TRANSACTION;
 UPDATE animals SET species = 'unspecified';
-ROLLBACK;
-END;
+ROLLBACK TRANSACTION;
 
-/* set animals with mon to digimon */
-BEGIN;
+BEGIN TRANSACTION;
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
-COMMIT;
-END;
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT TRANSACTION;
 
-/* set pokemon for all null */
-BEGIN;
-UPDATE animals SET species = 'pokemon' WHERE species is NULL;
-COMMIT;
-END;
+BEGIN TRANSACTION;
+DELETE FROM animals;
+ROLLBACK TRANSACTION;
 
+BEGIN TRANSACTION;
+DELETE FROM animals WHERE date_of_birth > 'Jan 1, 2022';
+SAVEPOINT DELETE_DATE; 
+UPDATE animals SET weight_kg = (weight_kg * -1);
+ROLLBACK TO DELETE_DATE;
+UPDATE animals SET weight_kg = (weight_kg * -1) WHERE weight_kg < 0;
+COMMIT TRANSACTION;
 
-/* delete all animals and rollback*/
-BEGIN;
-DELETE from animals *;
-SELECT * from animals;
-ROLLBACK;
-END;
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered, MAX(escape_attempts) FROM animals GROUP BY neutered;
+SELECT neutered, MIN(weight_kg), MAX(weight_kg) from animals GROUP BY neutered;
+SELECT neutered, AVG(escape_attempts) FROM animals WHERE EXTRACT(year FROM date_of_birth) BETWEEN 1990 AND 2000 GROUP BY neutered;
 
-SELECT * from animals; /* making sure all data still exist */
+/* Query multiple tables */
 
-/* delete animals born after Jan 1st, 2022*/
-BEGIN;
-DELETE from animals where date_of_birth >= '2022-01-01';
-COMMIT;
-SAVEPOINT SP1;
-UPDATE animals SET weight_kg = weight_kg * - 1;
-ROLLBACK SP1;
-UPDATE animals SET weight_kg = weight_kg * - 1 where weight_kg < 0;
-COMMIT;
-END;
+SELECT animals.name, owners.full_name FROM animals
+    INNER JOIN owners
+    ON owners.id = animals.owner_id
+    AND owners.full_name = 'Melody Pond';
+
+SELECT animals.name, species.name FROM animals
+    INNER JOIN species
+    ON species.id = animals.species_id
+    AND species.name = 'Pokemon';
+
+SELECT owners.full_name, animals.name FROM owners
+    LEFT JOIN animals
+    ON owners.id = animals.owner_id;
+
+SELECT species.name, COUNT(*) FROM animals
+    FULL OUTER JOIN species
+    ON species.id = animals.species_id
+    GROUP BY species.id;
+
+SELECT animals.name, species.name FROM animals
+    INNER JOIN owners ON owners.id = animals.owner_id
+    INNER JOIN species ON species.id = animals.species_id
+    WHERE owners.full_name = 'Jennifer Orwell'
+    AND species.name = 'Digimon';
+
+SELECT animals.name, animals.escape_attempts FROM animals
+    INNER JOIN owners ON owners.id = animals.owner_id
+    WHERE owners.full_name = 'Dean Winchester'
+    AND animals.escape_attempts = 0;
+
+SELECT owners.full_name, COUNT(animals.owner_id) FROM animals 
+    FULL OUTER JOIN owners 
+    ON animals.owner_id = owners.id
+    GROUP BY owners.id;
+
+/* Add "join table" for visits */
+
+SELECT animals.name, visits.date_of_visit AS last_visit FROM visits
+    INNER JOIN vets ON vets.id = visits.vets_id
+    INNER JOIN animals ON animals.id = visits.animals_id
+    WHERE vets.name = 'William Tatcher'
+    GROUP BY animals.name, visits.date_of_visit
+    ORDER BY last_visit DESC LIMIT 1;
+
+SELECT COUNT(DISTINCT visits.animals_id) FROM visits
+    INNER JOIN vets ON vets.id = visits.vets_id
+    WHERE vets.name = 'Stephanie Mendez';
+
+SELECT vets.name, species.name FROM vets 
+    LEFT JOIN specialization ON specialization.vets_id = vets.id
+    LEFT JOIN species ON species.id = specialization.species_id;
+
+SELECT animals.name, visits.date_of_visit FROM visits
+    INNER JOIN vets ON vets.id = visits.vets_id
+    INNER JOIN animals ON animals.id = visits.animals_id
+    WHERE vets.name = 'Stephanie Mendez'
+    AND visits.date_of_visit BETWEEN 'Apr 1, 2020' AND 'Aug 30, 2020';
+
+SELECT animals.name, COUNT(visits.animals_id) AS visit_count FROM visits
+    INNER JOIN vets ON vets.id = visits.vets_id
+    INNER JOIN animals ON animals.id = visits.animals_id
+    GROUP BY animals.name, visits.animals_id
+    ORDER BY visit_count DESC LIMIT 1;
+
+SELECT animals.name, visits.date_of_visit AS first_visit FROM visits
+    INNER JOIN vets ON vets.id = visits.vets_id
+    INNER JOIN animals ON animals.id = visits.animals_id
+    WHERE vets.name = 'Maisy Smith'
+    GROUP BY animals.name, visits.date_of_visit
+    ORDER BY first_visit LIMIT 1;
+
+SELECT * FROM visits
+    INNER JOIN vets ON vets.id = visits.vets_id
+    INNER JOIN animals ON animals.id = visits.animals_id
+    ORDER BY visits.date_of_visit DESC LIMIT 1;
+
+SELECT COUNT(visits.animals_id) FROM visits
+    INNER JOIN vets ON vets.id = visits.vets_id
+    INNER JOIN animals ON animals.id = visits.animals_id
+    INNER JOIN specialization ON specialization.vets_id = vets.id
+    WHERE specialization.species_id <> animals.species_id;
+
+SELECT species.name, COUNT(visits.animals_id) AS species_count FROM visits
+    INNER JOIN vets ON vets.id = visits.vets_id
+    INNER JOIN animals ON animals.id = visits.animals_id
+    INNER JOIN species ON species.id = animals.species_id
+    WHERE vets.name = 'Maisy Smith'
+    GROUP BY species.name
+    ORDER BY species_count DESC LIMIT 1;
+
+/* Performace Audit */
+
+explain analyze SELECT COUNT(*) FROM visits where animals_id = 4;
+explain analyze SELECT * FROM visits where vets_id = 2;
+explain analyze SELECT * FROM owners where email = 'owner_18327@mail.com';
